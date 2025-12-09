b220ad21abfd871750788ac9f5d3a413c267ca79
Silas Santos <93554313+silass4ntos@users.noreply.github.com>
2025-12-09 18:10:37 +0000

    create

---
+++ minecraft/run_crafty.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /workspaces/MineServer/minecraft
+source .venv/bin/activate 
+cd crafty-4 
+exec python3 main.py 
---
+++ minecraft/run_crafty_service.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /workspaces/MineServer/minecraft
+source .venv/bin/activate 
+cd crafty-4 
+python3 main.py -d
